c7cca29b5b91e21f36bd10ddc7ee34bc10dcfc39
bulatz <bulat.ziganshin@gmail.com>
2015-11-20 22:37:40 +0300

    Added MSVC compile script

---
+++ compile.cmd
@@ -0,0 +1,11 @@
+@echo off
+set options=ntt.cpp
+set options_ms=-MP -Gy -GR- -nologo -Fo%TEMP%\ -Fp%TEMP%\ %options% user32.lib shell32.lib ole32.lib advapi32.lib %* -link -LARGEADDRESSAWARE
+set options_ms=-GL -Fa %options_ms%
+set options_ms_cl=-O2 -EHsc %options_ms%
+set options_ms_x86=-MACHINE:x86 -SUBSYSTEM:CONSOLE,5.01
+set options_ms_x64=-MACHINE:x64 -SUBSYSTEM:CONSOLE,5.02
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+cl -Fentt64m.exe %options_ms_cl% %options_ms_x64%
+del *.exe.bak *.obj *.res >nul 2>nul
