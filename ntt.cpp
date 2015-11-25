cbf8921d2bca4099c5c1c010f805ec252b81f1ef
bulatz <bulat.ziganshin@gmail.com>
2015-11-25 05:37:42 +0300

    NTT: process 512 bytes for every element (i.e. 512 MiB total)

--- ntt.cpp
+++ ntt.cpp
@@ -64,10 +64,11 @@ ElementT GF_Mul64 (ElementT X, ElementT Y)
 template <ElementT P>
 ElementT GF_Mul32 (ElementT X, ElementT Y)
 {
+    // invP32 := (2**64)/P - 2**32  :  if 2**31<P<2**32, then 2**32 < (2**64)/P < 2**33, and invP32 is a 32-bit value
     const DoubleElementT estInvP = ((DoubleElementT(1)<<63) / P) << 1;                          // == invP & (~1)
-    const ElementT       invP32  = ElementT(estInvP*P > (estInvP+1)*P? estInvP : estInvP+1);    // we can't use 1<<64 for exact invP computation, so we add one when required in other way
+    const ElementT       invP32  = ElementT(estInvP*P > (estInvP+1)*P? estInvP : estInvP+1);    // we can't use 1<<64 for exact invP computation so, when required, we add one in other way
     DoubleElementT res = DoubleElementT(X)*Y;
-    res  -=  ((res + (res>>32)*invP32) >> 32) * P;
+    res  -=  ((res + (res>>32)*invP32) >> 32) * P;    // The same as res -= ((res*invP) >> 64) * P, where invP = (2**64)/P, but optimized for 32-bit computations
     return ElementT(res>=P? ElementT(res)-P : res);
 }
 
@@ -80,52 +81,58 @@ ElementT GF_Mul32 (ElementT X, ElementT Y)
 
 
 template <size_t N, typename T, T P>
-class DanielsonLanczos {
-   DanielsonLanczos<N/2,T,P> next;
+class DanielsonLanczos 
+{
+    DanielsonLanczos<N/2,T,P> next;
 public:
-   void apply(T* data, T root) {
-      T root_sqr = GF_Mul<P> (root, root);  // first root of power N of 1
-      if (N>8192) {
-          #pragma omp task
-          next.apply (data,   root_sqr);
-          #pragma omp task
-          next.apply (data+N, root_sqr);
-          #pragma omp taskwait
-      } else {
-          next.apply (data,   root_sqr);
-          next.apply (data+N, root_sqr);
-      }
+    void apply (T* data, size_t SIZE, T root) 
+    {
+        T root_sqr = GF_Mul<P> (root, root);  // first root of power N of 1
+        if (N>8192) {
+            #pragma omp task
+            next.apply (data,   SIZE/2, root_sqr);
+            #pragma omp task
+            next.apply (data+N, SIZE/2, root_sqr);
+            #pragma omp taskwait
+        } else {
+            next.apply (data,   SIZE/2, root_sqr);
+            next.apply (data+N, SIZE/2, root_sqr);
+        }
 
-      T root_i = root;   // first root of power 2N of 1 
-      for (size_t i=0; i<N; i++) {
-        T temp    = GF_Mul<P> (root_i, data[i+N]);
-        data[i+N] = GF_Sub<P> (data[i], temp);
-        data[i]   = GF_Add<P> (data[i], temp);
-        root_i    = GF_Mul<P> (root_i, root);  // next root of power 2N of 1
-      }
-   }
+        T root_i = root;   // first root of power 2N of 1 
+        for (size_t i=0; i<N; i++) {
+            for (size_t k=0; k<SIZE; k+=N) {
+                T temp    = GF_Mul<P> (root_i, data[k+N]);
+                data[k+N] = GF_Sub<P> (data[k], temp);
+                data[k]   = GF_Add<P> (data[k], temp);
+            }
+            root_i = GF_Mul<P> (root_i, root);  // next root of power 2N of 1
+        }
+    }
 };
  
 template<typename T, T P>
 class DanielsonLanczos<0,T,P> {
 public:
-   void apply(T* data, T root) { }
+   void apply(T* data, size_t SIZE, T root) { }
 };
 
 
 
 template <size_t Exp, typename T, T P>
-class NTT {
-   enum { N = 1<<Exp };
-   DanielsonLanczos<N,T,P> recursion;
+class NTT 
+{
+    enum { N = 1<<Exp };
+    DanielsonLanczos<N,T,P> recursion;
 public:
-   void ntt(T* data) {
-      T root = 1557;                      // init 'root' with root of 1 of power 2**20 in GF(0xFFF00001)
-      for (int i=20; --i>Exp; )
-          root = GF_Mul<P> (root, root);  // find root of 1 of power 2N
-      scramble(data,N);
-      recursion.apply(data,root);
-   }
+    void ntt (T* data, size_t SIZE) 
+    {
+        T root = 1557;                      // init 'root' with root of 1 of power 2**20 in GF(0xFFF00001)
+        for (int i=20; --i>Exp; )
+            root = GF_Mul<P> (root, root);  // find root of 1 of power 2N
+        scramble(data,N);
+        recursion.apply(data,SIZE,root);
+    }
 };
 
 
@@ -176,13 +183,13 @@ int main()
     // Test_GF_Mul32<P>(); 
     // FindRoot<ElementT,P>(20);  // prints 1557
 
-    ElementT *data = new ElementT[1<<20];
-    for (int i=0; i<(1<<20); i++)
+    const size_t SIZE = 128<<20;  // 512 MB
+    ElementT *data = new ElementT[SIZE];
+    for (int i=0; i<SIZE; i++)
         data[i] = i;
     NTT<19,ElementT,P> transformer;
     #pragma omp parallel num_threads(16)
     #pragma omp single
-    for (int i=0; i<100; i++)
-        transformer.ntt(data);
+    transformer.ntt(data,SIZE);
     return 0;
 }
