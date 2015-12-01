0dfbc84a9c4011281ce5644510bd0ba29bd91c1d
bulatz <bulat.ziganshin@gmail.com>
2015-12-01 12:23:46 +0300

    RS: replaced MT PRNG with LCG

--- compile.cmd
+++ compile.cmd
@@ -1,5 +1,6 @@
 @echo off
-set options=ntt.cpp
+set name=rs
+set options=%name%.cpp
 set options_ms=-MP -Gy -GR- -nologo -Fo%TEMP%\ -Fp%TEMP%\ %options% user32.lib shell32.lib ole32.lib advapi32.lib %* -link -LARGEADDRESSAWARE
 set options_ms=-GL %options_ms%
 set options_ms_cl=-O2 -EHsc %options_ms%
@@ -7,9 +8,9 @@ set options_ms_x86=-MACHINE:x86 -SUBSYSTEM:CONSOLE,5.01
 set options_ms_x64=-MACHINE:x64 -SUBSYSTEM:CONSOLE,5.02
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
-cl -Fentt64m.exe -Fa %options_ms_cl% %options_ms_x64%
+cl -Fe%name%64m.exe -Fa %options_ms_cl% %options_ms_x64%
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-cl -Fentt32m.exe %options_ms_cl% %options_ms_x86%
+cl -Fe%name%32m.exe %options_ms_cl% %options_ms_x86%
 
 del *.exe.bak *.obj *.res >nul 2>nul
--- rs.cpp
+++ rs.cpp
@@ -10,102 +10,22 @@
 
 typedef unsigned char byte;
 
-/************************************************/
-/** Random number generator -> 32bits          **/
-/** Mersenne twister code                      **/
-/************************************************/
+/*****************************************************************/
+/** Random number generator -> 32bits                           **/
+/** https://en.wikipedia.org/wiki/Linear_congruential_generator **/
+/*****************************************************************/
 
-/* A C-program for MT19937: Integer     version                   */
-/*  genrand() generates one pseudorandom unsigned integer (32bit) */
-/* which is uniformly distributed among 0 to 2^32-1  for each     */
-/* call. sgenrand(seed) set initial values to the working area    */
-/* of 624 words. Before genrand(), sgenrand(seed) must be         */
-/* called once. (seed is any 32-bit integer except for 0).        */
-/*   Coded by Takuji Nishimura, considering the suggestions by    */
-/* Topher Cooper and Marc Rieffel in July-Aug. 1997.              */
+static unsigned long last_rnd = 0;
 
-/* This library is free software; you can redistribute it and/or   */
-/* modify it under the terms of the GNU Library General Public     */
-/* License as published by the Free Software Foundation; either    */
-/* version 2 of the License, or (at your option) any later         */
-/* version.                                                        */
-/* This library is distributed in the hope that it will be useful, */
-/* but WITHOUT ANY WARRANTY; without even the implied warranty of  */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.            */
-/* See the GNU Library General Public License for more details.    */
-/* You should have received a copy of the GNU Library General      */
-/* Public License along with this library; if not, write to the    */
-/* Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA   */
-/* 02111-1307  USA                                                 */
-
-/* Copyright (C) 1997 Makoto Matsumoto and Takuji Nishimura.       */
-/* Any feedback is very welcome. For any question, comments,       */
-/* see http://www.math.keio.ac.jp/matumoto/emt.html or email       */
-/* matumoto@math.keio.ac.jp                                        */
-
-/* Period parameters */
-#define MT_N 624
-#define MT_M 397
-#define MATRIX_A 0x9908b0df   /* constant vector a */
-#define UPPER_MASK 0x80000000 /* most significant w-r bits */
-#define LOWER_MASK 0x7fffffff /* least significant r bits */
-
-/* Tempering parameters */
-#define TEMPERING_MASK_B 0x9d2c5680
-#define TEMPERING_MASK_C 0xefc60000
-#define TEMPERING_SHIFT_U(y)  (y >> 11)
-#define TEMPERING_SHIFT_S(y)  (y << 7)
-#define TEMPERING_SHIFT_T(y)  (y << 15)
-#define TEMPERING_SHIFT_L(y)  (y >> 18)
-
-static unsigned long mt[MT_N]; /* the table for the state vector  */
-static int mti=MT_N+1; /* mti==MT_N+1 means mt[MT_N] is not initialized */
-
-/* initializing the table with a NONZERO seed */
-void sgenrand(unsigned long seed)
+void sgenrand (unsigned long seed)
 {
-    /* setting initial seeds to mt[MT_N] using         */
-    /* the generator Line 25 of Table 1 in          */
-    /* [KNUTH 1981, The Art of Computer Programming */
-    /*    Vol. 2 (2nd Ed.), pp102]                  */
-    mt[0]= seed & 0xffffffff;
-    for (mti=1; mti<MT_N; mti++)
-        mt[mti] = (69069 * mt[mti-1]) & 0xffffffff;
+    last_rnd = seed;
 }
 
 unsigned int genrand()
 {
-    unsigned int y;
-    static unsigned long mag01[2]={0x0, MATRIX_A};
-    /* mag01[x] = x * MATRIX_A  for x=0,1 */
-
-    if (mti >= MT_N) { /* generate MT_N words at one time */
-        int kk;
-
-        if (mti == MT_N+1)   /* if sgenrand() has not been called, */
-            sgenrand(4357); /* a default initial seed is used   */
-
-        for (kk=0;kk<MT_N-MT_M;kk++) {
-            y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-            mt[kk] = mt[kk+MT_M] ^ (y >> 1) ^ mag01[y & 0x1];
-        }
-        for (;kk<MT_N-1;kk++) {
-            y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-            mt[kk] = mt[kk+(MT_M-MT_N)] ^ (y >> 1) ^ mag01[y & 0x1];
-        }
-        y = (mt[MT_N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
-        mt[MT_N-1] = mt[MT_M-1] ^ (y >> 1) ^ mag01[y & 0x1];
-
-        mti = 0;
-    }
-
-    y = mt[mti++];
-    y ^= TEMPERING_SHIFT_U(y);
-    y ^= TEMPERING_SHIFT_S(y) & TEMPERING_MASK_B;
-    y ^= TEMPERING_SHIFT_T(y) & TEMPERING_MASK_C;
-    y ^= TEMPERING_SHIFT_L(y);
-
-    return y;
+    last_rnd = 29943829*last_rnd + 1013904223;
+    return last_rnd;
 }
 
 double double_genrand() {
@@ -387,10 +307,10 @@ void ifft_inc(int *vect, int n)
 
     int i,j;
     int number=n/2;
-    
+
     int step=1;
     int mult=15;
-    
+
     int *root=exp + (1<<16);
     while (number>0)
     {
@@ -774,4 +694,3 @@ int main(int argc, char *argv[])
     // end;
     return 0;
 }
-
