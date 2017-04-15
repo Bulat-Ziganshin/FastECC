51a4efc0c0f51233589e3aa45c14882ffbd2f8eb
bulatz <bulat.ziganshin@gmail.com>
2017-04-15 21:35:03 +0300

    Updated compile.cmd to compile main.cpp instead of NTT.cpp as main file of the project

--- compile.cmd
+++ compile.cmd
@@ -1,6 +1,6 @@
 @echo off
 set name=ntt
-set options=%name%.cpp
+set options=main.cpp
 set options_ms=-MP -Gy -GR- -nologo -Fo%TEMP%\ -Fp%TEMP%\ %options% user32.lib shell32.lib ole32.lib advapi32.lib %* -link -LARGEADDRESSAWARE
 set options_ms=-GL %options_ms%
 set options_ms_cl=-O2 -EHsc %options_ms%
@@ -8,11 +8,13 @@ set options_ms_x86=-MACHINE:x86 -SUBSYSTEM:CONSOLE,5.01
 set options_ms_x64=-MACHINE:x64 -SUBSYSTEM:CONSOLE,5.02
 
 ::call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+::call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64 -no_logo
 cl -Fe%name%64m.exe -Fa %options_ms_cl% %options_ms_x64%
 
 ::call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+::call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" -arch=x86 -host_arch=x64 -no_logo
 cl -Fe%name%32m.exe %options_ms_cl% %options_ms_x86%
 
 del *.exe.bak *.obj *.res >nul 2>nul
